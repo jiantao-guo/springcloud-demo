5b11a72dbf091780b8e9aa9a3ee24401059427e9
jiantao.guo <jiantao.guo@hand-china.com>
2019-12-30 17:57:21 +0800

    config server commit

---
+++ demo-config-server/src/test/java/com/gg/democonfigserver/DemoConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gg.democonfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
